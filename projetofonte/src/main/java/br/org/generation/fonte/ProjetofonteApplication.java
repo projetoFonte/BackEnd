6abee89a74bd6746d05733b6d34afdfef2411bd8
levi <nanowebcontato@gmail.com>
2022-01-21 10:34:12 -0300

    Criação do projeto

---
+++ projetofonte/src/main/java/br/org/generation/fonte/ProjetofonteApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.fonte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofonteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofonteApplication.class, args);
+	}
+
+}
---
+++ projetofonte/src/test/java/br/org/generation/fonte/ProjetofonteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.fonte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofonteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
