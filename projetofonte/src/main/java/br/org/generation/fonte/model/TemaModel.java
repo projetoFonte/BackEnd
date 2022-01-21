efed9f69b427e1c05ed88a3f1a291e001c99ac8d
Lucas Silva Braga <lucas.sbraga14@gmail.com>
2022-01-21 11:53:26 -0300

    Modelo do tema - Task_04

---
+++ projetofonte/src/main/java/br/org/generation/fonte/model/TemaModel.java
@@ -0,0 +1,54 @@
+package br.org.generation.fonte.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity 
+@Table(name = "tb_tema")
+
+public class TemaModel {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) // 
+	private Long id;
+	
+	@NotBlank(message = " É necessário definir a categoria! ")
+	@Size(min = 10, max = 255, message = " É necessário que informe a categoria do seu tema, utilizando no mínimo 10 caractéres. ")
+	private String categoria;
+	
+	@NotBlank(message = "É necessário informar a descrição do seu tema! ")
+	@Size(min = 10, max = 255, message = " É necessário que informe a descrição do seu tema, utilizando no mínimo 10 caractéres. ")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+}
