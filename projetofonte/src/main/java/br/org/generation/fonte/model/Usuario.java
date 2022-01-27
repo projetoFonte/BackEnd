4f248242dd72b0e9e5351d2fae289281ff40a8e7
Natielle Arruda <78966664+NatielleArruda@users.noreply.github.com>
2022-01-27 13:40:54 -0300

    Criação Usuario Model

---
+++ projetofonte/src/main/java/br/org/generation/fonte/model/Usuario.java
@@ -0,0 +1,78 @@
+package br.org.generation.fonte.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="tb_usuario")
+public class Usuario {
+	
+	@Id 
+	@GeneratedValue (strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="É necessário cadastrar um email válido.")
+	@Size(min = 4, max = 255, message = " É necessário cadastrar um email válido.. ")
+	private String email;
+	
+	@NotBlank(message="É necessário cadastrar um nome válido.")
+	@Size(min = 4, max = 255, message = " É necessário que seu nome tenha no minimo 4 caracteres. ")
+	private String nome;
+	
+	@NotBlank(message="É necessário cadastrar uma senha válida.")
+	@Size(min = 6, max = 255, message = " É necessário que informe uma senha válida. ")
+	private String senha;
+	
+	private String foto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
+
+
+
+
+}
