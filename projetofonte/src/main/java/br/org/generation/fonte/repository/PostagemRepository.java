4ba8b2c8c8133c4a32712795ce0345bc528e778f
unknown <gabriel,maia1333@gmail.com>
2022-01-27 12:05:34 -0300

    Criação da Model Postagem.

--- projetofonte/src/main/java/br/org/generation/fonte/repository/PostagemRepository.java
+++ projetofonte/src/main/java/br/org/generation/fonte/repository/PostagemRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.org.generation.fonte.model.PostagemModel;
+
 @Repository
 public interface PostagemRepository extends JpaRepository<PostagemModel, Long> 
 {	
