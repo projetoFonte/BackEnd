38ce23d09b9d06ceba5a2a18e923121b923719ae
levi <nanowebcontato@gmail.com>
2022-02-02 16:23:01 -0300

    Implementação da Security

--- projetofonte/src/main/java/br/org/generation/fonte/controller/UsuarioController.java
+++ projetofonte/src/main/java/br/org/generation/fonte/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,8 +35,14 @@ public class UsuarioController {
 	
 	@GetMapping("/all")
 	public ResponseEntity <List<Usuario>> getAll(){
-		
 		return ResponseEntity.ok(usuarioRepository.findAll());
+	}
+	
+	@GetMapping("/id/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable Long id){
+		return usuarioRepository.findById(id)
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
 	}
 	
 	@PostMapping("/logar")
