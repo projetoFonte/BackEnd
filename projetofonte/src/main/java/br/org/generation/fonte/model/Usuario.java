cfdf6c205f3d32fde93f52a5ae4173cc89b61164
levi <nanowebcontato@gmail.com>
2022-02-04 12:19:59 -0300

    Configuração Swagger do projeto

--- projetofonte/src/main/java/br/org/generation/fonte/model/Usuario.java
+++ projetofonte/src/main/java/br/org/generation/fonte/model/Usuario.java
@@ -15,6 +15,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuario")
 public class Usuario {
@@ -23,6 +25,7 @@ public class Usuario {
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Schema(example = "email@email.com.br")
 	@NotBlank(message = "É necessário cadastrar um email válido.")
 	@Email(message = "É necessário cadastrar um email válido.")
 	private String usuario;
